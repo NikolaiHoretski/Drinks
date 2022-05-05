34661a82ff990a618328a9456793aad3a3798346
NikolaiHoretski <nikolai.horetski@gmail.com>
2022-05-05 14:04:24 +0400

    Added KindOfCoffee, KindOfJuice, KindofJuice enam

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,19 @@
 public class Test {
     public static void main(String[] args) {
-        Cup <Tea> cup1 = new Cup<>(new Tea());
-        Cup <Coffee> cup2 = new Cup<>(new Coffee());
-        Cup <Juice> cup3 = new Cup<>(new Juice());
+        Cup<Tea> cup1 = new Cup<>(new Tea());
+        Cup<Coffee> cup2 = new Cup<>(new Coffee());
+        Cup<Juice> cup3 = new Cup<>(new Juice());
         cup1.drink.typeOfDrink();
+        for (KindOfTea kindOfTea : KindOfTea.values()) {
+            System.out.println(kindOfTea);
+        }
         cup2.drink.typeOfDrink();
+        for (KindOfCoffee kindOfCoffee : KindOfCoffee.values()) {
+            System.out.println(kindOfCoffee);
+        }
         cup3.drink.typeOfDrink();
-
-
+        for (KindOfJuice kindOfJuice : KindOfJuice.values()) {
+            System.out.println(kindOfJuice);
+        }
     }
 }
